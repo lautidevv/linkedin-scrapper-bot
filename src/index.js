005c048786e01833afe1418b9859ffe7f84b4404
lautidevv <lautideveloper@gmail.com>
2024-02-11 19:50:50 -0300

    feat(hi): add logic to hi bot

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const {Client, IntentsBitField} = require('discord.js')
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+})
+
+client.on('ready', (c) => {
+    console.log(`✅ ${c.user.tag} is online `)
+})
+
+client.on('messageCreate', (message) => {
+    console.log(message.content)
+    if (message.author.bot) return
+    if (message.content === 'Hola' || message.content === 'hola') {
+        message.reply('Hola!!')
+    }
+})
+client.login(process.env.TOKEN)
