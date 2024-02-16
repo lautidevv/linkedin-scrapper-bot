aeedf57b46ecc4b685f4813df6c2f74212cc0617
facuagueria <facundoagueria1@gmail.com>
2024-02-15 23:35:57 -0300

    chore: config eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:prettier/recommended', 'prettier', 'eslint:recommended'],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  rules: {
+    'no-var': 'error',
+    semi: 'error',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'no-multi-spaces': 'error',
+    'space-in-parens': 'error',
+    'no-multiple-empty-lines': 'error',
+    'prefer-const': 'error',
+  },
+};
